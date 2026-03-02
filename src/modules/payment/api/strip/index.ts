9ed3b2122a54c37fabf1707f13592a4fa7e1d649
libra <libra@libras-MacBook-Pro.local>
2026-03-02 11:28:07 +0700

    chore: applied url testing

--- src/modules/payment/api/strip/index.ts
+++ src/modules/payment/api/strip/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
+const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export async function stripPayment(){
-	const req = await axios.post('http://localhost:8900/stripe/api/strip/payment')
+	const req = await axios.post(`${SERVER_URL}/stripe/api/strip/payment`)
 	return req.data
 }
