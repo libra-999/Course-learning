fa603bf2090d51bce96e245f02f022be0943a9e1
libra <libra@libras-MacBook-Pro.local>
2026-03-06 12:26:50 +0700

    modified page homepage

---
+++ src/app/components/card/Cardbox.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Notebook } from '@element-plus/icons-vue'
+import type { Component } from 'vue'
+
+defineProps<{
+	title: string
+	content: string
+	path: string
+	icon?: Component
+}>()
+defineModel()
+</script>
+
+<template>
+	<RouterLink :to="path">
+		<div class="relative overflow-hidden rounded-sm border border-gray-200 transition-all duration-150 hover:-translate-y-2 hover:shadow-xl hover:bg-gray-100/70 bg-gray-100/50 p-2 text-start">
+			<div class="flex h-[180px] flex-col justify-between rounded-md p-6">
+				<el-icon size="48">
+					<component v-if="icon" :is="icon"/>
+					<Notebook v-else />
+				</el-icon>
+				<div class="space-y-2">
+					<h3 class="font-bold">{{ title }}</h3>
+					<p class="text-sm text-muted-foreground">
+						{{ content }}
+					</p>
+				</div>
+			</div>
+		</div>
+	</RouterLink>
+</template>
+
+<style scoped></style>
--- src/app/layout/DefaultLayout.vue
+++ src/app/layout/DefaultLayout.vue
@@ -3,6 +3,8 @@ import { decrypt } from '@/app/utils/hash.ts'
 import { loginStore, type User } from '@/app/store/auth.ts'
 import { ref } from 'vue'
 import route from '@/route'
+import Cardbox from '@/app/components/card/Cardbox.vue'
+import { DataLine, DocumentAdd } from '@element-plus/icons-vue'
 
 const checkLocalstorage = localStorage.getItem('userLogin')
 const userData = ref<User>({ username: '', password: '' })
@@ -30,26 +32,41 @@ try {
 }
 </script>
 <template>
-	<div class="my-5 text-black mx-2">
-		<p><strong>Modules</strong></p>
+	<div class="w-max px-3 py-5 text-[#112a46]">
+		<div
+			class="mx-auto w-full py-10 border-b-[#112a46] border-2 border-r-transparent border-t-transparent border-l-transparent"
+		>
+			<p class="uppercase font-bold text-center text-sm mb-3">
+				Vue 3 Learning Tutorial
+			</p>
+			<p class="uppercase text-center text-5xl font-bold">
+				From Zero to Expert
+			</p>
+		</div>
+		<div class="w-[1000px] mt-5 grid grid-cols-3 gap-4 overflow-hidden">
+			<!-- Test -->
+			<Cardbox
+				title="Test"
+				content="Just a normal click box with auto created box number "
+				path="/test"
+				:icon="DocumentAdd"
+			>
+			</Cardbox>
+			<!-- Book -->
+			<Cardbox
+				title="Book API"
+				content="Google book API with  , list product book as list and view detail of book "
+				path="/books"
+			/>
+			<!-- Payment -->
+			<Cardbox
+				title="Payment Type"
+				content="Learned the structure and fields should be have when implement payment feature into project"
+				path="/payment"
+				:icon="DataLine"
+			/>
+		</div>
 	</div>
-	<nav>
-		<RouterLink
-			class="text-white border-2 rounded-sm bg-blue-400 w-max p-2 mx-2"
-			to="/test"
-			>Test
-		</RouterLink>
-		<RouterLink
-			class="text-white border-2 rounded-sm bg-blue-400 w-max p-2 mx-2"
-			to="/books"
-			>Book Store
-		</RouterLink>
-		<RouterLink
-			class="text-white border-2 rounded-sm bg-blue-400 w-max p-2 mx-2"
-			to="/payment"
-			>Payment
-		</RouterLink>
-	</nav>
 	<RouterView />
 </template>
 
