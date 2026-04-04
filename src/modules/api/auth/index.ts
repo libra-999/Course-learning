f60c6b5b6d7009423fbc889ce6eccac588e0cca9
libra <libra@libras-MacBook-Pro.local>
2026-04-04 11:39:21 +0700

    feat: adding auth scan

--- src/modules/api/auth/index.ts
+++ src/modules/api/auth/index.ts
@@ -2,6 +2,47 @@ import apiRequest from '@/app/utils/request'
 import type { LoginRequest } from '@/modules/types/auth'
 
 export async function login(payload: LoginRequest): Promise<any | undefined> {
-	const login = await apiRequest.post("/auth/login", payload)
+	const login = await apiRequest.post('/auth/login', payload)
 	return login.data
+}
+
+export async function generateQR(): Promise<any> {
+	const generate = await apiRequest.get('/auth/qr/generate')
+	return generate.data
+}
+export async function getQR(qrToken: string): Promise<any> {
+	const getQR = await apiRequest.get('/auth/qr', {
+		headers: {
+			qrToken: qrToken,
+		},
+	})
+	return getQR.data
+}
+export async function scanQR(qrToken: string): Promise<any> {
+	const scanQR = await apiRequest.post(
+		'/auth/qr/scan',
+		{}, // none body
+		{
+			headers: {
+				qrToken: qrToken,
+			},
+		},
+	)
+	return scanQR.data
+}
+export async function confirmLogin(qrToken: string, _payload: any): Promise<any> {
+	const confirm = await apiRequest.post('auth/qr/confirm',_payload, {
+		headers :{
+			qrToken: qrToken
+		}
+	})
+	return confirm.data
+}
+export async function cancel(qrToken: string): Promise<any> {
+	const cancel = await apiRequest.post('auth/qr/cancel',{}, {
+		headers :{
+			qrToken: qrToken
+		}
+	})
+	return cancel.data
 }
