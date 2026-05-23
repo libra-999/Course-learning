cf22eaee2c256a42a58521ee0e820a968be3d6c7
libra-999 <brali6597@gmail.com>
2026-05-23 14:10:25 +0700

    bug

--- src/modules/api/bot/index.ts
+++ src/modules/api/bot/index.ts
@@ -1,7 +1,6 @@
 import apiRequest from "@/app/utils/request";
 import axios from "axios";
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export async function BotApi(contents: any, deviceName: string): Promise<any>{
     const generateContent = await apiRequest.post("/api/chat/bot/generate-content", {
         contents: contents,
@@ -11,5 +10,5 @@ export async function BotApi(contents: any, deviceName: string): Promise<any>{
 }
 
 export async function healthServer() :Promise<any> {
-    return await axios.get(`${SERVER_URL}/`)
+    return await axios.get("/")
 }
